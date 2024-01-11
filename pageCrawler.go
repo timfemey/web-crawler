def6eff1ad130dfa9304baf8d87ade302270ddb2
timfemey <isholaobafemi@gmail.com>
2024-01-11 15:15:26 +0100

    modified pageCrawler removed Comments

--- pageCrawler.go
+++ pageCrawler.go
@@ -10,11 +10,11 @@ func CrawlWebPage(wg *sync.WaitGroup, WebsitesChannel chan string,
 	pendingSitesChannel chan int) {
 	crawled := 0
 
-	// for url := range WebsitesChannel {
-	// 	extractContent(url,crawledSitesChannel)
-	// 	pendingSitesChannel <- -1
-	// 	crawled++
-	// }
+	for url := range WebsitesChannel {
+		extractContent(url, crawledSitesChannel)
+		pendingSitesChannel <- -1
+		crawled++
+	}
 
 	fmt.Println("Crawled ", crawled, " web sites")
 
